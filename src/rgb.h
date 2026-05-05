a4717fc09db449ce05f56cb4db69420bee11f3b0
DJBeethoven9 <20184311@stu.uob.edu.bh>
2026-05-05 23:21:53 +0300

    removing some files

--- src/rgb.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Adafruit_NeoPixel.h>
-
-#define LED_PIN  48
-#define NUM_LEDS 1
-
-void rgbSetup();
-void rgbLoop();
--- src/web_server.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <WebServer.h>
-
-extern WebServer server;
-
-void webServerSetup();
-void webServerLoop();
